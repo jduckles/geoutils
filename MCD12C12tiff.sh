1c13263e248057cf3cb7dda5e7064f2bdb46b6f1
Jonah Duckles <jduckles@gmail.com>
2014-05-05 11:24:05 -0500

    Extract layers of MCD12C1 to tiffs

---
+++ MCD12C12tiff.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Extract Land Cover Type 1 to tif files from MCD12C1
+
+
+FNAME=$1
+
+LABEL=${FNAME:0:13}
+
+gdal_translate HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Land_Cover_Type_1_Percent ${LABEL}_Land_Cover_Type_1_Percent.tif
+gdal_translate HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Majority_Land_Cover_Type_1_Assessment ${LABEL}_Majority_Land_Cover_Type_1_Assessment.tif
+gdal_translate HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Majority_Land_Cover_Type_1 ${LABEL}_Majority_Land_Cover_Type_1.tif
+
+
+
