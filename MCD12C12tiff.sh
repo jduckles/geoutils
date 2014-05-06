7201b872e7fecc2248d0a39a2d5cfb19d3f64dc5
Jonah Duckles <jduckles@gmail.com>
2014-05-06 13:27:18 -0500

    Compare landcover

--- MCD12C12tiff.sh
+++ MCD12C12tiff.sh
@@ -7,9 +7,11 @@ FNAME=$1
 
 LABEL=${FNAME:0:13}
 
-gdal_translate HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Land_Cover_Type_1_Percent ${LABEL}_Land_Cover_Type_1_Percent.tif
-gdal_translate HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Majority_Land_Cover_Type_1_Assessment ${LABEL}_Majority_Land_Cover_Type_1_Assessment.tif
-gdal_translate HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Majority_Land_Cover_Type_1 ${LABEL}_Majority_Land_Cover_Type_1.tif
+CREATEOPTS="-co COMPRESS=LZW"
+
+gdal_translate ${CREATEOPTS} HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Land_Cover_Type_1_Percent ${LABEL}_Land_Cover_Type_1_Percent.tif
+gdal_translate ${CREATEOPTS} HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Majority_Land_Cover_Type_1_Assessment ${LABEL}_Majority_Land_Cover_Type_1_Assessment.tif
+gdal_translate ${CREATEOPTS} HDF4_EOS:EOS_GRID:"${FNAME}":MOD12C1:Majority_Land_Cover_Type_1 ${LABEL}_Majority_Land_Cover_Type_1.tif
 
 
 
